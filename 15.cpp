c8c9ed47828b84917ac5ff9ce5836058a6007fbb
beyzassevval <151019571+beyzassevval@users.noreply.github.com>
2023-11-17 17:31:23 +0300

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,14 @@
+	#include <iostream>
+	using namespace std;
+	//dairenin alaný
+	
+	int main(){
+		double pi=3.1415;
+		double yaricap=13;
+		double alan;
+		cout<< "dairenin alani"<<"\n";
+		cout<< "--------------"<<"\n";
+		alan=pi*yaricap*yaricap;
+		cout<<alan<<"cm2"<<"\n";
+		return 0;
+	}
